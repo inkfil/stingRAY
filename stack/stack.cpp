c37cf9a8fa9ecc86621bd4ae25696bde4a5e1865
ronaktheshinigami <ronakjainrjrjrj@gmail.com>
2020-09-07 18:55:26 +0530

    added some, more to be added

---
+++ stack/stack.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define Max 1000
+
+//Stack using Array as base structure
+class StackArr{
+  public:
+    int top;
+};
+
+//Stack using Linked List as base structure
+class StackLL{
+
+};
+
+int main(int argc, char** argv){
+  
+  return 0;
+}
